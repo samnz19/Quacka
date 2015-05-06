7bcccd8c81f162269fcb64b2fbd10c1f0c370867
user <user>
2015-05-07 11:16:30 +1200

    init setup of solution

---
+++ Quacka/Quacka/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Quacka.MvcApplication" Language="C#" %>
---
+++ Quacka/Quacka/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Quacka.Startup))]
+namespace Quacka
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
