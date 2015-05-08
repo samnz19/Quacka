98e1cc283b8b2eaf9c596adfc87cbb639746ced2
Rich Churcher <rich.churcher@gmail.com>
2015-05-08 13:42:40 +1200

    Rename/refactor plus Flocking

---
+++ Quacka/Quacka/Models/ShowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Quacka.Models
+{
+    public class ShowViewModel
+    {
+        public bool IsFollowing { get; set; }
+        public string UserName { get; set; }
+        public IEnumerable<Quack> Quacks { get; set; } 
+    }
+}
